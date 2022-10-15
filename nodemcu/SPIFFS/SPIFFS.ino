069bfd2c0a7a607809075ce1f5faa0accb5b170d
OmerHalil <gamega55834@gmail.com>
2022-10-16 00:22:55 +0300

    LittleFS | code updfates

--- nodemcu/SPIFFS/SPIFFS.ino
+++ nodemcu/SPIFFS/SPIFFS.ino
@@ -1,30 +1,36 @@
-#include "LittleFS.h"
- 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Hellow World"); 
-  if(!LittleFS.begin()){
-    Serial.println("An Error has occurred while mounting LittleFS");
-    return;
-  }
-}
- 
-void loop() {
-  Serial.println("Hello");
+#include "LittleFS.h";
+
+void readFile()
+{
   File file = LittleFS.open("/config.txt", "r");
   if(!file){
     Serial.println("Failed to open file for reading");
   }
   else
   {
-    Serial.println("File Content:");
+    String data = "";
     while(file.available()){
-      Serial.write(file.read());
+      data += file.readString();
     }
+    
+    Serial.println(data);
     file.close();
   }
-  
-  delay(3000); 
+}
 
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hellow World"); 
   
+  if(!LittleFS.begin()){
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
+  }
+
+}
+ 
+void loop() {
+  readFile();
+  delay(5000);
 }
