02080af57aebc235e4daf0b408d47b06c65176c1
OmerHalil <gamega55834@gmail.com>
2022-10-15 21:18:06 +0300

    Re make all giles

--- nodemcu/SPIFFS/SPIFFS.ino
+++ nodemcu/SPIFFS/SPIFFS.ino
@@ -1,59 +1,30 @@
-#include<ESP8266WiFi.h>
-#include<FS.h>
-bool FS_AVALIABLE = false;
-
-
-void initFileSystem()
-{
- if(SPIFFS.begin()){
-  FS_AVALIABLE = true;
-  Serial.println("file true");
- }
- else {
-  FS_AVALIABLE = false;
-  Serial.println("file folse");
- }
+#include "LittleFS.h"
+ 
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hellow World"); 
+  if(!LittleFS.begin()){
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
+  }
 }
-void readFile(String filename){
-    Serial.println("Init file");
-
-  File f=SPIFFS.open(filename,"r");
-  if(!f)
-  {
-    Serial.println("file open failed");
+ 
+void loop() {
+  Serial.println("Hello");
+  File file = LittleFS.open("/config.txt", "r");
+  if(!file){
+    Serial.println("Failed to open file for reading");
   }
   else
   {
-    Serial.println("Reading Data from file");
-    int i = 0;
-    for(i=0;i<f.size();i++)
-    {
-      Serial.print((char)f.read());
+    Serial.println("File Content:");
+    while(file.available()){
+      Serial.write(file.read());
     }
-    f.close();
+    file.close();
   }
-}
+  
+  delay(3000); 
 
-void write(){}
-void create(){}
-
-void setup()
-{
-  delay(1000);
-  Serial.begin(115200);
-  Serial.println("Start Working");
-  initFileSystem();
-  if(SPIFFS.format())
-{
-Serial.println("File system Formatted");
-}
-else
-{
-Serial.println("File system formatting error");
-}
-  readFile("/data.txt");
-}
-void loop() {
-  readFile("/data.txt");
-  delay(1000);
+  
 }
