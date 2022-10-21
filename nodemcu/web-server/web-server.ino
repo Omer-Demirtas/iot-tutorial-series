5eaee6982debc847c7973c5499ced651a8459d6d
OmerHalil <gamega55834@gmail.com>
2022-10-21 20:39:19 +0300

    Web Server | handle web server

--- nodemcu/web-server/web-server.ino
+++ nodemcu/web-server/web-server.ino
@@ -1,14 +1,39 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include "FileServicce.h";
+#include "LittleFS.h";
 
-String ssid = "NAME";
-String password = "PASSWORD";
+String ssid = "OMERDEMIRTAS-LA 3167";
+String password = "592^06Qy";
 
 ESP8266WebServer server(80);
 
+String getFile(String path)
+{
+  File file = LittleFS.open(path, "r");
+  String data = ""; 
+
+  if(!file){
+    Serial.println("Failed to open file for reading");
+  }
+  else
+  {
+    while(file.available()){
+      data += file.readString();
+    }
+    file.close();
+
+    return data;
+  }
+  return data;
+}
+
 void setup() {
   Serial.begin(115200);
+
+  if(!LittleFS.begin()){
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
+  }
 
   WiFi.begin(ssid, password);
 
@@ -21,9 +46,12 @@ void setup() {
   Serial.println("connected..! ");
   Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
 
-  server.on("/", handleOnConnect);
-  server.onNotFound(handleNotFound);
+  //server.on("/", handleOnConnect);
+  //server.onNotFound(handleNotFound);
+ // Route for root / web page
+  server.on("/", HTTP_GET, handleOnConnect);
 
+  
   server.begin();
   
   Serial.println("HTTP server started");
@@ -33,7 +61,7 @@ void loop() {
 }
 
 void handleOnConnect() {
-  server.send(200, "text/html", "Hello"); 
+  server.send(200, "text/html", getFile("/index.html")); 
 }
 
 void handleNotFound(){
