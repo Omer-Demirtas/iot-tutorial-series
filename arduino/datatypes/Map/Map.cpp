d6f43669572397f756805f3d10dfc5eeee7e3ceb
OmerHalil <gamega55834@gmail.com>
2022-10-21 13:45:33 +0300

    Map | get mthod bug fix

--- arduino/datatypes/Map/Map.cpp
+++ arduino/datatypes/Map/Map.cpp
@@ -2,34 +2,26 @@
 #include "Arduino.h";
 
 
-template <typename T, typename U> Map<T, U>::Map()
-{
-    Serial.println("Map created!!");
-}
+template <typename T, typename U> Map<T, U>::Map() { }
 
 template <typename T, typename U> void Map<T, U>::append(T key, U value)
 {
     size++;
     keys[size] = key;
     values[size] = value;
-    Serial.print(key);
-    Serial.println(value);
 }
 
 template <typename T, typename U> U Map<T, U>::get(T key)
 {
-  Serial.println(key);
+  U u;
   for (int i = 0; i < size; i++)
   {
     if(keys[i] == key)
     {
-      Serial.print("found with  ");
-      Serial.print(i);
-
       return values[i];
     }
   }
-  return;
+  return u;
 }
 
 template class Map<String, int>;
