fe2f209e01fa8180ac67847fccaf9069b55ccec6
Omer-Demirtas <gamega55834@gmail.com>
2022-11-06 22:43:29 +0300

    Interrupt code update

--- arduino/Interrupt/timer_interrupt/timer_interrupt.ino
+++ arduino/Interrupt/timer_interrupt/timer_interrupt.ino
@@ -1,15 +1,27 @@
-#include <Servo.h>
 
-Servo servo1;
 
-void setup() {
-  servo1.attach(9);
+void setup()
+{
+  Serial.begin(9600);
+    
+  cli();
+
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+  OCR1A = 15624;
+  /* Bir saniye aralıklar için zaman sayıcısı ayarlandı */
+  TCCR1B |= (1 << WGM12);
+  /* Adımlar arasında geçen süre kristal hızının 1024'e bölümü olarak ayarlandı */
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  TIMSK1 |= (1 << OCIE1A);
+  sei();  
 }
 
+ISR(TIMER1_COMPA_vect){
+  Serial.println("Hello, World!");
+}
 void loop() 
 {
-  servo1.write(90);
-  delay(1000);  
-  servo1.write(0);
-  delay(1000);
+ 
 }
